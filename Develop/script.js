de93078007591f4dad54a227a4645d9d1262849c
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-05-07 12:52:27 -0600

    updated PW generatro commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,11 @@
-var lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
-var lowerAlphaArray = lowerAlpha.split("");
-var upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var upperAlphaArray = upperAlpha.split("");
+var lowerCaseAlpha = "abcdefghijklmnopqrstuvwxyz";
+var lowerCaseAlphaArray = lowerCaseAlpha.split("");
+var upperCaseAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var upperCaseAlphaArray = upperCaseAlpha.split("");
 var numbers = "0123456789";
 var numbersArray = numbers.split("");
-var symbols = "~!@#$%^&_-+=";
-var symbolsArray = symbols.split("");
+var specialCharacters = "~!@#$%^&_-+=";
+var specialCahractersArray = specialCharacters.split("");
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -14,22 +14,24 @@ function writePassword() {
   var allCharacters = [];
   var passwordText = document.querySelector("#password");
   var characterCount = prompt(
-    "How many characters would you like your password to be? \n8-128"
+    "How many characters would you like your password to be? \n(8-128)"
   );
   if (characterCount < 8 || characterCount > 128) {
-    alert("the password character count must be over 8\nPlease re-start");
+    alert(
+      "the password character count must be over 8 and under 128.\nPlease re-start"
+    );
   } else {
-    if (confirm("Contain uppercase letters?")) {
-      Array.prototype.push.apply(allCharacters, upperAlphaArray);
+    if (confirm("Do you want it to contain uppercase letters?")) {
+      Array.prototype.push.apply(allCharacters, upperCaseAlphaArray);
     }
-    if (confirm("Contain lowercase letters?")) {
-      Array.prototype.push.apply(allCharacters, lowerAlphaArray);
+    if (confirm("Do you want it to contain lowercase letters?")) {
+      Array.prototype.push.apply(allCharacters, lowerCaseAlphaArray);
     }
-    if (confirm("Contain numbers?")) {
+    if (confirm("Do you want it to contain numbers?")) {
       Array.prototype.push.apply(allCharacters, numbersArray);
     }
-    if (confirm("Contain symbols?")) {
-      Array.prototype.push.apply(allCharacters, symbolsArray);
+    if (confirm("Do you want it to contain special characters?")) {
+      Array.prototype.push.apply(allCharacters, specialCahractersArray);
     }
     if (allCharacters.length === 0) {
       alert(
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Password Generator Starter Code
+# 🔐 Password Generator Starter Code
+
+- As a developer, I want to fabricate a random password generater that provides greater security for users
+
+## Acceptance Criteria
+
+- It's done when, a user clicks the 'Generate Password' button, then they are prompted with various password criteria
+
+- It's done when, a user gets an option to choose a character count, then they are able to choose between 8-128 characters
+
+- It is done when, a user is prompted whether or not they desire to include uppercase, lowercase, numeric, and/or special characters, then they are able to submit their choices
+
+- It is done when, a user answers all of the prompts, then the random password is displayed in the 'Your Secure Password' box and includes all of the options the user chose
+
+---
+
+© 2022 addiguskey
